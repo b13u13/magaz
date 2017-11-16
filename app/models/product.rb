5b90f9c7ca6bb0572110abbd4f35f3c96c66c2e6
aovsiy200 <Alexander_Ovsiychuk@comcast.com>
2017-11-16 18:03:31 +0200

    Tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
 	validates :price, numericality: { greather_than_or_equal_to: 0.01 }
 	validates :title, uniqueness: true
 	validates :image_url, allow_blank: true, format: {
-
+		with: %r{\.(.gif|jpg|png)\Z}i,
+		message: 'URL должен указывать на изображения формата GIF, JPG или PNG.'
 	}
 end
