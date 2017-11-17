01ddbd41931fb81b764935dc5eed61895c9f739b
aovsiy200 <Alexander_Ovsiychuk@comcast.com>
2017-11-17 17:04:05 +0200

    Add few tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ApplicationRecord
 	validates :title, :description,  :image_url, presence: true
-	validates :price, numericality: { greather_than_or_equal_to: 0.01 }
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :title, uniqueness: true
 	validates :image_url, allow_blank: true, format: {
-		with: %r{\.(.gif|jpg|png)\Z}i,
+		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'URL должен указывать на изображения формата GIF, JPG или PNG.'
 	}
 end
