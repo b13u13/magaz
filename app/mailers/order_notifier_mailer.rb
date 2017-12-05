d8706d7dd51447119e29338df71d3f5cf3e3d43b
aovsiy200 <Alexander_Ovsiychuk@comcast.com>
2017-12-05 17:24:00 +0200

    Add emails

---
+++ app/mailers/order_notifier_mailer.rb
@@ -0,0 +1,26 @@
+class OrderNotifierMailer < ApplicationMailer
+
+  default from: 'EBB <easy.busybuy@gmial.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier_mailer.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: 'Подтверждение заказа'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier_mailer.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: 'Заказ из отправлен'
+  end
+end
