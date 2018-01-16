84ca7030cf7252aace3a9ede38d5c82be3bfd2c3
aovsiy200 <Alexander_Ovsiychuk@comcast.com>
2018-01-16 16:47:31 +0200

    Fixed integration test

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -3,44 +3,49 @@ require 'pry'
 
 class UserStoriesTest < ActionDispatch::IntegrationTest
 	fixtures :products
+	include ActiveJob::TestHelper
 
-	test "buying a product" do 
-		LineItem.delete_all
-		Order.delete_all
+	test "buying a product" do
+		start_order_count = Order.count
 		ruby_book = products(:ruby)
 
-		get "/"
+		get store_url
 		assert_response :success
 		assert_template "index"
 
-		post "/line_items", params: { product_id: ruby_book.id  }		
-		assert_redirected_to "/"
-		binding.pry
+		post line_items_url, params: { product_id: ruby_book.id  }
+		assert_redirected_to store_url
+
 		cart = Cart.find(session[:cart_id])
 		assert_equal 1, cart.line_items.size
 		assert_equal ruby_book, cart.line_items[0].product
 
 		get "/orders/new"
 		assert_response :success
-		assert_template "new"
-binding.pry
-		post '/orders', params: { order: { name: 		"Dave Thomas",
-										  addres: 	"123 The Street",
-										  email: 	"dave@example.com",
-										  pay_type:  "Check" }
-								}		  
-		
+		perform_enqueued_jobs do
+			post "/orders", params: {
+					order: {
+									name: "Dave Thomas",
+									address: "123 The Street",
+									email: "dave@example.com",
+									pay_type: "Check"
+									}
+		}
+			follow_redirect!
+
+
 		assert_response :success
-		
+
+
 		cart = Cart.find(session[:cart_id])
 		assert_equal 0, cart.line_items.size
 
-		orders = Order.all
-		assert_equal 1, orders.size
-		order = orders[0]
+		assert_equal start_order_count + 1, Order.count
+
+		order = Order.last
 
 		assert_equal "Dave Thomas", order.name
-		assert_equal "123 The Street", order.addres
+		assert_equal "123 The Street", order.address
 		assert_equal "dave@example.com", order.email
 		assert_equal "Check", order.pay_type
 
@@ -51,9 +56,9 @@ binding.pry
 
 		mail = ActionMailer::Base.deliveries.last
 		assert_equal ["dave@example.com"], mail.to
-		assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
-		assert_equal "Pragmatic Store Order Confirmation", mail.suject
-
+		assert_equal 'EBB <depot@example.com>', mail[:from].value
+		assert_equal "Подтверждение заказа в Pragmatic Store", mail.subject
+		end
 	end
 end
 
