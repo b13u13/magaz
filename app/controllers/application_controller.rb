c3d304a161e939e7083abe915e5efdf8eeef9ad1
aovsiy200 <Alexander_Ovsiychuk@comcast.com>
2018-07-31 15:41:00 +0300

    Add admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_date_loaded
+  before_action :authorize
 
 
 
@@ -9,5 +10,14 @@ class ApplicationController < ActionController::Base
   def set_date_loaded
   	@date_loaded = Time.now
   end
+
+
+  protected
+
+    def authorize
+    	unless User.find_by(id: session[:user_id])
+    		redirect_to login_url, notice: 'Please login'
+    	end
+    end
 
 end
